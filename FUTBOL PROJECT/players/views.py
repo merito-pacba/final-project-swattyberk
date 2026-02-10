8bb96b3f4d9b38e93ae94c755b16ee49acdd04cc
swattyberk <swat4bak@hotmail.com>
2026-02-10 21:51:13 +0100

    fix the views and urls code

--- FUTBOL PROJECT/players/views.py
+++ FUTBOL PROJECT/players/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Player  # Oyuncu modelini içeri aktarıyoruz
 
-# Create your views here.
+def home(request):
+    # Veritabanındaki tüm oyuncuları çekiyoruz
+    players = Player.objects.all() 
+    # Bu oyuncuları 'index.html' sayfasına gönderiyoruz
+    return render(request, 'index.html', {'players': players})
