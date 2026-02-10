cd8962fe1493e00947d943f7f59cd574cb969a12
swattyberk <swat4bak@hotmail.com>
2026-02-10 22:10:36 +0100

    fix media url and add messi foto.

--- FUTBOL_PROJECT/scout_project/urls.py
+++ FUTBOL_PROJECT/scout_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from players import views # Uygulama adın players olduğu için
+from django.conf import settings # Bunu ekle
+from django.conf.urls.static import static # Bunu ekle
+from players import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'), # views içindeki home fonksiyonuna bağladık
-]
+    path('', views.home, name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Bu satırı ekle
