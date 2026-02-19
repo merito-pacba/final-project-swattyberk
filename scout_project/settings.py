fbc7e103114ddfdc659a6e78b5ee5b799371618e
swattyberk <swat4bak@hotmail.com>
2026-02-20 00:00:01 +0100

    change static code in settings.py

--- scout_project/settings.py
+++ scout_project/settings.py
@@ -70,10 +70,8 @@ USE_I18N = True
 USE_TZ = True
 
 # STATİK VE MEDYA DOSYALARI (Fotoğrafların görünmesi için şart)
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
